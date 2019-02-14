e39e12d5946e92d469220109293876b81d038f00
pecadorcelestial <unit_zero@hotmail.com>
2019-02-14 09:46:17 -0600

    Preparing the package for publishing.

--- index.ts
+++ index.ts
@@ -1,42 +1,42 @@
 import { Loading, Animate, FlipCard, FrontContent, BackContent } from './lib/animations/animations';
-// import { Badge, IconBadge, NotificationBadge } from './lib/badges/badges';
-// import { Button, IconButton, RoundButton } from './lib/buttons/buttons';
-// import { Calendar } from './lib/calendar/calendar';
-// import { BasicCard } from './lib/cards/cards';
-// import { CheckBox } from './lib/checkboxes/checkboxes';
-// import { BasicSelect, AdvancedSelect } from './lib/dropdownlists/dropdownlists';
-// import { CircularGraph } from './lib/graphs/graphs';
-// import { Icon } from './lib/icons/icons';
-// import { MenuWithBlur } from './lib/menus/menus';
-// import { Modal, ModalHeader, ModalTitle, ModalBody, ModalFooter } from './lib/modals/modals';
-// import { RadioButton, RadioButtonsGroup } from './lib/radiobuttons/radiobuttons';
-// import { BasicTextBox } from './lib/textboxes/textboxes';
-// import { Tabs } from './lib/tabs/tabs';
-// import { ToastNotification } from './lib/toastnotifications/toastnotifications';
-// import ToolTip from './lib/tooltip/tooltip';
+import { Badge, IconBadge, NotificationBadge } from './lib/badges/badges';
+import { Button, IconButton, RoundButton } from './lib/buttons/buttons';
+import { Calendar } from './lib/calendar/calendar';
+import { Card } from './lib/cards/cards';
+import { CheckBox } from './lib/checkboxes/checkboxes';
+import { BasicSelect, AdvancedSelect } from './lib/dropdownlists/dropdownlists';
+import { CircularGraph } from './lib/graphs/graphs';
+import { Icon } from './lib/icons/icons';
+import { MenuWithBlur } from './lib/menus/menus';
+import { Modal, ModalHeader, ModalTitle, ModalBody, ModalFooter } from './lib/modals/modals';
+import { RadioButton, RadioButtonsGroup } from './lib/radiobuttons/radiobuttons';
+import { TextBox } from './lib/textboxes/textboxes';
+import { Tabs } from './lib/tabs/tabs';
+import { ToastNotification } from './lib/toastnotifications/toastnotifications';
+import { ToolTip } from './lib/tooltip/tooltip';
 
 export = {
     Animate,
-    // Badge,
-    // Button,
-    // Calendar,
-    // Card: BasicCard,
-    // CheckBox,
-    // CircularGraph,
+    Badge,
+    Button,
+    Calendar,
+    Card,
+    CheckBox,
+    CircularGraph,
     FlipCard, FrontContent, BackContent,
-    // Icon,
-    // IconBadge,
-    // IconButton,
+    Icon,
+    IconBadge,
+    IconButton,
     Loading,
-    // MenuWithBlur,
-    // Modal, ModalHeader, ModalTitle, ModalBody, ModalFooter,
-    // NotificationBadge,
-    // Select: BasicSelect, AdvancedSelect,
-    // RadioButton, 
-    // RadioButtonsGroup,
-    // RoundButton,
-    // Tabs,
-    // TextBox: BasicTextBox,
-    // ToastNotification,
-    // ToolTip
+    MenuWithBlur,
+    Modal, ModalHeader, ModalTitle, ModalBody, ModalFooter,
+    NotificationBadge,
+    Select: BasicSelect, AdvancedSelect,
+    RadioButton, 
+    RadioButtonsGroup,
+    RoundButton,
+    Tabs,
+    TextBox,
+    ToastNotification,
+    ToolTip
 };
